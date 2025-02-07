bd631792847951a11401254fb9a9a124e45c9b93
Anderson Lopes <developesanderson@gmail.com>
2025-02-07 12:53:55 -0400

    Refactor user registration to use RegisterUseCase class and PrismaUsersRepository for improved structure and dependency management

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -8,25 +7,28 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  email,
-  name,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+// D - Dependency Inversion Principle (DIP)
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: { email },
-  })
+export class RegisterUseCase {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(private usersRepository: any) {}
 
-  if (userWithSameEmail) {
-    throw new Error('Email already exists')
+  async execute({ email, name, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('Email already exists')
+    }
+    // const prismaUsersRepository = new PrismaUsersRepository()
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
   }
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
