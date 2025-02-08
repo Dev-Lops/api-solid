eaa8d7029a2674b129978cb6e18da104d8384ca3
Anderson Lopes <developesanderson@gmail.com>
2025-02-07 20:58:24 -0400

    Add custom error handling for validation and internal server errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -28,7 +28,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         message: err.message,
       })
     }
-    return reply.status(500).send() // TODO: fix me
+    throw err
   }
   return reply.status(201).send()
 }
