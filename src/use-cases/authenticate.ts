8c9f6910a3a9feef4cbd4fdfac91ebc321667747
Anderson Lopes <developesanderson@gmail.com>
2025-02-11 06:55:06 -0400

    Implement authentication use case and add routes for user sessions

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -12,7 +12,7 @@ interface AuthenticateUseCaseResponse {
   user: User
 }
 
-export class authenticateUseCase {
+export class AuthenticateUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
   async execute({
